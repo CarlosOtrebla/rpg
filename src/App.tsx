6a8d656459ede918899715e261cdf469b9645e07
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-04-20 13:13:27 -0300

    create hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import * as C from './App.styles'
 import { Character } from './components/Character'
+import { useCharacter } from './hooks/useCharacter'
 
 const App = () => {
+
+  const Char = useCharacter();
+  
+
   return (
     <C.Container>
       <C.Map>
-        <Character x={6} y={4} size={20}/>
+        <Character x={Char.x} y={Char.y} size={20}/>
       </C.Map>
     </C.Container>
 
