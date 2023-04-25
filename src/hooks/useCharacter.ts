2c4d5d0f6d48e5dff9c0a3bd65a7766bddce76c2
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-04-25 10:33:11 -0300

    Update useCharacter.ts

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -1,46 +1,62 @@
-import { useState } from "react" 
+import { useState } from 'react';
+import { CharacterSides } from '../types/CharacterSides';
+import { mapSpots } from '../data/mapSpots';
 
-export const useCharacter = () => {
+export const useCharacter = (propName: string) => {
+  const [name, setName] = useState(propName);
+  const [pos, setPos] = useState({ x: 3, y: 5 });
+  const [side, setSide] = useState<CharacterSides>('down');
+
+  const moveLeft = () => {
+    setPos(pos => ({
+      x: canMove(pos.x - 1, pos.y) ? pos.x - 1 : pos.x,
+      y: pos.y,
+    }));
+    setSide('left');
+  };
+
+  const moveRight = () => {
+    setPos(pos => ({
+      x: canMove(pos.x + 1, pos.y) ? pos.x + 1 : pos.x,
+      y: pos.y,
+    }));
+    setSide('right');
+  };
+
+  const moveDown = () => {
+    setPos(pos => ({
+      x: pos.x,
+      y: canMove(pos.x, pos.y + 1) ? pos.y + 1 : pos.y
+    }));
+    setSide('down');
+  };
+
+  const moveUp = () => {
+    setPos(pos => ({
+      x: pos.x,
+      y: canMove(pos.x, pos.y - 1) ? pos.y - 1 : pos.y
+    }));
+    setSide('up');
+  };
+
+  const canMove = (x: number, y: number) => {
+    console.log(x, y);
     
-    const [pos, setPos] = useState({x: 1, y: 1})
-   
+    if(mapSpots[y]!=undefined && mapSpots[x] !== undefined) {
 
-    const moveLeft = () => {
-        setPos(pos => ({
-            x: pos.x - 1,
-            y: pos.y,
-            
-        }))
-    };
-
-    const moveRight = () => {
-        setPos(pos => ({
-            x: pos.x + 1,
-            y: pos.y
-        }))
-    };
-
-    const moveUp = () => {
-        setPos(pos => ({
-            x: pos.x,
-            y: pos.y - 1
-        }))
-    };
-
-    const moveDown = () => {
-        setPos(pos => ({
-            x: pos.x,
-            y: pos.y + 1
-        }))
+      return (mapSpots[y][x] === 1);
     }
+    return false;
+  };
 
-    return {
-        x: pos.x,
-        y: pos.y,
-        moveDown,
-        moveLeft,
-        moveRight,
-        moveUp,
-      
-    }
-}
+  return {
+    name,
+    x: pos.x,
+    y: pos.y,
+    side,
+    moveDown,
+    moveLeft,
+    moveRight,
+    moveUp,
+  };
+};
