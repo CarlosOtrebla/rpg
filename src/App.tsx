5d6b305b727a5e1574d470a124f371218089370c
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-04-25 10:32:55 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,62 @@
-import { useEffect } from 'react'
-import * as C from './App.styles'
-import { Character } from './components/Character'
-import { useCharacter } from './hooks/useCharacter'
+/* eslint-disable indent */
+import React from 'react';
+import { useEffect } from 'react';
+import * as C from './App.styles';
+import { Character } from './components/Character';
+import { useCharacter } from './hooks/useCharacter';
 
 
 const App = () => {
-
-  const Char = useCharacter();
+  const Char = useCharacter('Otrebla');
+  const Char2 = useCharacter('João');
 
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown)
-
+    window.addEventListener('keydown', handleKeyDown);
   }, []);
 
-  const  handleKeyDown = (e: KeyboardEvent) => {
-    console.log(e.code)
-    switch(e.code) {
+  const handleKeyDown = (e: KeyboardEvent) => {
+    
+    switch (e.code) {
       case 'KeyA':
-      case 'ArrowLeft':
         Char.moveLeft();
-      break;
-      case 'KeyW':
-      case 'ArrowUp':
-        Char.moveUp();
-      break;
-      case 'KeyD':
-      case 'ArrowRight':
-        Char.moveRight();
-      break;
-      case 'KeyS':
-      case 'ArrowDown':
-        Char.moveDown();
-      break;
-    }
-  }
+        break;
+      case 'ArrowLeft':
+        Char2.moveLeft();
+        break;
 
-  
+      case 'KeyW':
+        Char.moveUp();
+        break;
+      case 'ArrowUp':
+        Char2.moveUp();
+        break;
+
+      case 'KeyD':
+        Char.moveRight();
+        break;
+      case 'ArrowRight':
+        Char2.moveRight();
+        break;
+
+      case 'KeyS':
+        Char.moveDown();
+        break;
+      case 'ArrowDown':
+        Char2.moveDown();
+        break;
+    }
+  };
 
   return (
+  
     <C.Container>
       <C.Map>
-        <Character x={Char.x} y={Char.y} size={30}/>
+        
+        <Character x={Char.x} y={Char.y} size={30} side={Char.side} name={Char.name} />
+        <Character x={Char2.x} y={Char2.y} size={30} side={Char2.side} name={Char2.name}/>
       </C.Map>
     </C.Container>
+  );
+};
 
-  )
-}
-
-export default App
+export default App;
