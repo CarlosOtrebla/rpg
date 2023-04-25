f81a0b98ad1c00dd75b072c35b6033db52006e60
Carlos Alberto Lopes Duarte <carlosotrebla@gmail.com>
2023-04-25 10:32:59 -0300

    Update index.tsx

--- src/components/Character/index.tsx
+++ src/components/Character/index.tsx
@@ -1,20 +1,32 @@
-import * as C from './styles'
+import React from 'react';
+import * as C from './styles';
+import { CharacterSides } from '../../types/CharacterSides';
 
 type Props = {
-    x: number;
-    y: number;
-    size: number
-}
+  x: number;
+  y: number;
+  size: number;
+  side: CharacterSides;
+  name: string;
+};
 
-export const Character = ({size, x, y}: Props) => {
-    // const size = 30;
-    return (
-        <C.Container
-            size={size}
-            left={x*size}
-            top={y*size}
-            >
-            
-        </C.Container>
-    )
-}
+export const Character = ({ size, x, y, side, name }: Props) => {
+  const sides = {
+    down: 0,
+    left: -30,
+    right: -60,
+    up: -90,
+  };
+
+  return (
+    <C.Container
+      size={size}
+      left={x * size}
+      top={y * size}
+      sidePos={sides[side] ?? 0}
+    >
+      <C.charName>{name}</C.charName>
+      
+    </C.Container>
+  );
+};
